da3ac7ead46acdec943ff88ca44a46d639e2af18
Roman Khocholava <khocholavaroman@gmail.com>
2018-02-28 11:30:05 +0200

    Projekt mitler no adaptive header

---
+++ js/common.js
@@ -0,0 +1,20 @@
+$(function() {
+
+	$('#my-menu').mmenu({
+		extensions: [ 'theme-black', 'effect-menu-slide', 'pagedin-black' ],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	}); 
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('close:finish', function(){
+		$('.hamburger').removeClass('is-active');
+	})
+
+});
