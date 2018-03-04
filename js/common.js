e4556e6f065d7bb46bbfe0ca1a4a9d8889241644
Roman Khocholava <khocholavaroman@gmail.com>
2018-03-04 20:06:26 +0200

    Footer / Completed adaptive site + preloader

--- js/common.js
+++ js/common.js
@@ -120,6 +120,17 @@ $(function() {
 		}
 	});
 
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+	});
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 	//Resize windiw
 	function onResize() {
 		$('.carousel-services-content').equalHeights();
@@ -128,3 +139,7 @@ $(function() {
 
 
 });
+
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
+})
