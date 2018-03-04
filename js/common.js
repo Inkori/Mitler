706eecc30d12c0e2f41c15bf17e5351fa7e4553d
Roman Khocholava <khocholavaroman@gmail.com>
2018-03-04 16:26:58 +0200

    Section partners-carousel

--- js/common.js
+++ js/common.js
@@ -97,6 +97,28 @@ $(function() {
 		return false;
 	});
 
+	$(".partners").owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
 
 	//Resize windiw
 	function onResize() {
